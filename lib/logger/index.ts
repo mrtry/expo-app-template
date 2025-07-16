4565b5adb07d99cb675b459faa7f1b5f29d9d9d5
Shinnosuke Yamamoto <yamamoto9302@gmail.com>
2025-07-16 18:20:33 +0900

    lib/logger/index.ts の作成

---
+++ lib/logger/index.ts
@@ -0,0 +1,22 @@
+export const Logger = {
+  log(...args: unknown[]): void {
+    console.log(...args);
+  },
+
+  warn(...args: unknown[]): void {
+    console.warn(...args);
+  },
+
+  error(...args: unknown[]): void {
+    console.error(...args);
+  },
+
+  logException(exception: unknown): void {
+    // ここでSentryなどのエラーレポートサービスに例外を報告します
+    // 例: Sentry.captureException(exception);
+
+    if (__DEV__) {
+      this.error(exception);
+    }
+  },
+};
